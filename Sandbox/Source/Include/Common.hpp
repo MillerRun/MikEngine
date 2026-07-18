ef6556c278164d9ebce11a1672aa0780ac6f9a69
MillerRun <mike@gonchar.org>
2026-07-18 15:13:44 +0300

    - Used assert in GLCHECK

--- Sandbox/Source/Include/Common.hpp
+++ Sandbox/Source/Include/Common.hpp
@@ -1,36 +1,31 @@
 #pragma once
 
-#include <format>
-#include <string>
-#include <string_view>
-#include <fstream>
+#include <Utils/Assert.hpp>
 
 // GLCHECK - checks for gl errors
 //
 #ifdef _DEBUG
 #  include <glad/glad.h>
-#  include <print>
-#  define GLCHECK( call )                                                                                                 \
-   do                                                                                                                     \
-   {                                                                                                                      \
-      while( glGetError() != GL_NO_ERROR );                                                                               \
-      call;                                                                                                               \
-      const GLenum eError = glGetError();                                                                                 \
-      if( eError != GL_NO_ERROR )                                                                                         \
-      {                                                                                                                   \
-         std::string sErrorText;                                                                                          \
-         switch( eError )                                                                                                 \
-         {                                                                                                                \
-         case GL_INVALID_ENUM: sErrorText = "GL_INVALID_ENUM"; break;                                                     \
-         case GL_INVALID_VALUE: sErrorText = "GL_INVALID_VALUE"; break;                                                   \
-         case GL_INVALID_OPERATION: sErrorText = "GL_INVALID_OPERATION"; break;                                           \
-         case GL_OUT_OF_MEMORY: sErrorText = "GL_OUT_OF_MEMORY"; break;                                                   \
-         case GL_INVALID_FRAMEBUFFER_OPERATION: sErrorText = "GL_INVALID_FRAMEBUFFER_OPERATION"; break;                   \
-         default: sErrorText = std::format( "0x{:o}", eError );                                                           \
-         }                                                                                                                \
-         std::println( stderr, "[ERROR OpenGL] {}:{}: {}(0x{:o}) at {}", __FILE__, __LINE__, sErrorText, eError, #call ); \
-         __debugbreak();                                                                                                  \
-      }                                                                                                                   \
+#  define GLCHECK( call )                                                                               \
+   do                                                                                                   \
+   {                                                                                                    \
+      while( glGetError() != GL_NO_ERROR );                                                             \
+      call;                                                                                             \
+      const GLenum eError = glGetError();                                                               \
+      if( eError != GL_NO_ERROR )                                                                       \
+      {                                                                                                 \
+         std::string sErrorText;                                                                        \
+         switch( eError )                                                                               \
+         {                                                                                              \
+         case GL_INVALID_ENUM: sErrorText = "GL_INVALID_ENUM"; break;                                   \
+         case GL_INVALID_VALUE: sErrorText = "GL_INVALID_VALUE"; break;                                 \
+         case GL_INVALID_OPERATION: sErrorText = "GL_INVALID_OPERATION"; break;                         \
+         case GL_OUT_OF_MEMORY: sErrorText = "GL_OUT_OF_MEMORY"; break;                                 \
+         case GL_INVALID_FRAMEBUFFER_OPERATION: sErrorText = "GL_INVALID_FRAMEBUFFER_OPERATION"; break; \
+         default: sErrorText = std::format( "0x{:o}", eError );                                         \
+         }                                                                                              \
+         MKASSERT( false, "0x{:o} - {} at {}", eError, sErrorText, #call );                             \
+      }                                                                                                 \
    } while( false )
 
 #else // not _DEBUG
